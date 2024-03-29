266572ba3dbf4239b9de050ccf01e405686d667e
Grzegorz Pustelnik <grzegorz.pustelnik@assaabloy.com>
2024-03-29 14:12:19 +0100

    Class methods and members

---
+++ Ch 1/class_start.py
@@ -0,0 +1,33 @@
+class Book:
+    # TODO: Properties defined at the class level are shared by all instances
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
+
+    # TODO: double-underscore properties are hidden from other classes
+
+    # TODO: create a class method
+
+    # TODO: create a static method
+
+    # instance methods receive a specific object instance as an argument
+    # and operate on data specific to that object instance
+    def set_title(self, newtitle):
+        self.title = newtitle
+
+    def __init__(self, title):
+        self.title = title
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError(f"{booktype} is not a valid book type")
+        else:
+            self.booktype = booktype
+
+
+
+# TODO: access the class attribute
+
+
+# TODO: Create some book instances
+b1 = Book("Titlel", "HARDCOVER")
+b2 = Book("Tittel", "COMIC")
+
+
+# TODO: Use the static method to access a singleton object
