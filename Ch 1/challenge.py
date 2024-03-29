ef27fca3f685fceeae3dc110421a9e0d4de5dc7e
Grzegorz Pustelnik <grzegorz.pustelnik@assaabloy.com>
2024-03-29 15:05:53 +0100

    Challenge_done

--- Ch 1/challenge.py
+++ Ch 1/challenge.py
@@ -1,2 +1,19 @@
 #Challenge: create a class to represent some information
 
+class Car:
+    def __init__(self, brand, model, body, year, horse_power, price) -> None:
+        self.brand = brand
+        self.model = model
+        self.body = body
+        self.year = year
+        self.horse_power = horse_power
+        self.price = price
+
+    def get_description(self):
+        return f"{self.brand}: {self.model}, {self.body}, {self.year}, {self.horse_power} -- {self.price} "
+
+merc = Car("Mercedes", "C AMG", "Sedan", 2023, "530 km", "450 000 zł")
+audi = Car("Audi", "RS6", "Avant", 2023, "650 km", "600 000 zł")
+
+print(merc.get_description())
+print(audi.get_description())
